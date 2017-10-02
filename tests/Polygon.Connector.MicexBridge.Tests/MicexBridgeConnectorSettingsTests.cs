e39dda876d806a5f76b7d6a5accdf6ee3f4f4f4b
Pavel Koryakin <koryakin@itglobal.ru>
2017-10-03 00:10:45 +0300

    MicexBridge connector tests

---
+++ tests/Polygon.Connector.MicexBridge.Tests/MicexBridgeConnectorSettingsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Polygon.Connector.MicexBridge.Tests
+{
+    public class MicexBridgeConnectorSettingsTests
+    {
+        [Fact]
+        public void ToStringWhenOnlyRequiredArgsProvided()
+        {
+            var settings = new MicexBridgeConnectorSettings(null, "127.0.0.2", "MCU0001", "777");
+            var connectionString = settings.ToString();
+            Assert.Equal(@"PASSWORD=777
+SERVER=127.0.0.2
+USERID=MCU0001
+", connectionString);
+        }
+    }
+}
